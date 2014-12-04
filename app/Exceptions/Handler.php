ef5f7d906e41a2621c8a89a3436ae32ec8c11221
Graham Campbell <graham@mineuk.com>
2014-12-04 20:37:00 +0000

    Added back the exception handler

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of StyleCI by Graham Campbell.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+
+namespace GrahamCampbell\StyleCI\Exceptions;
+
+use GrahamCampbell\Exceptions\ExceptionHandler;
+
+/**
+ * This is the exception hander class.
+ *
+ * @author    Graham Campbell <graham@mineuk.com>
+ * @copyright 2014 Graham Campbell
+ * @license   <https://github.com/GrahamCampbell/StyleCI/blob/master/LICENSE.md> AGPL 3.0
+ */
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var string[]
+     */
+    protected $dontReport = [
+        'Symfony\Component\HttpKernel\Exception\HttpException',
+    ];
+}
