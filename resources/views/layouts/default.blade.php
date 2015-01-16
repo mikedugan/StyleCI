ef6be322db2a30b5ac38119dca68f1e446c0612c
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-01-16 14:50:48 -0600

    Update layout to keep footer pushed

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,13 +5,13 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <title>{{ Config::get('graham-campbell/core::name') }} - @section('title')
 @show</title>
+<div id="wrap">
 @include('partials.header')
 </head>
 <body>
 @include('partials.navbar')
 @section('top')
 @show
-<div id="wrap">
 <div class="container">
 @include('partials.notifications')
 @section('content')
