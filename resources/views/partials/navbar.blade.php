1a3cc8249f94e0ba7d922f843f440eca60e30a7a
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-01-16 13:45:18 -0600

    Minor fixes

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,21 +1,21 @@
 <nav class="navbar navbar-inverse navbar-static-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="{{ route('home') }}">StyleCI</a>
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ route('home') }}">StyleCI</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="{{ route('repos_path') }}">Repositories</a></li>
+            </ul>
+            {{-- <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Login <span class="sr-only">(current)</span></a></li>
+            </ul> --}}
+        </div>
     </div>
-    <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav">
-        <li><a href="{{ route('repos_path') }}">Repositories</a></li>
-      </ul>
-      {{-- <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Login <span class="sr-only">(current)</span></a></li>
-      </ul> --}}
-    </div>
-  </div>
 </nav>
