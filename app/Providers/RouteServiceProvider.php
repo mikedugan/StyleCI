3d1f2cccf87075e78c789bd7d6e41561a06bacbf
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-01-24 01:21:26 -0600

    Fix closing function RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -56,6 +56,6 @@ class RouteServiceProvider extends ServiceProvider
     {
         $router->group(['namespace' => $this->namespace], function ($router) {
             require app_path('Http/routes.php');
-        }
+        });
     }
 }
