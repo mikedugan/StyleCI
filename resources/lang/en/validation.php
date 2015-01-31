fb28b53b56c31188621d12abcb52a37638dd1cb5
Graham Campbell <graham@mineuk.com>
2015-01-31 13:39:55 +0000

    Fixed order

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -3,8 +3,8 @@
 /*
  * This file is part of StyleCI.
  *
- * (c) Joseph Cohen <joseph.cohen@dinkbit.com>
  * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Joseph Cohen <joseph.cohen@dinkbit.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
