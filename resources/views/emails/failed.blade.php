f05790ddfed7a9e4d453411c81af7bc4aa65946c
Graham Campbell <graham@mineuk.com>
2015-01-31 21:10:09 +0000

    Removed old email template

--- resources/views/emails/failed.blade.php
+++
@@ -1,6 +0,0 @@
-@extends(Config::get('core.email'))
-
-@section('content')
-<p>The coding style analysis of your commit "{{ $commit }}" on repo "{{ $repo }}" revealed problems.
-<p>Click <a href="{{ $link }}">here</a> to see the details.</p>
-@stop
