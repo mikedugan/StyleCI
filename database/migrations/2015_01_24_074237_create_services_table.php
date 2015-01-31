8faae798b9b538ab3e91ae4daa00c139280b578d
Graham Campbell <graham@mineuk.com>
2015-01-31 19:43:28 +0000

    Added missing imports

--- database/migrations/2015_01_24_074237_create_services_table.php
+++ database/migrations/2015_01_24_074237_create_services_table.php
@@ -11,6 +11,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 /**
  * This is the create create services table migration class.
