66359d1d3f25d7f9f3a3fedc7a447141fcd5c226
Graham Campbell <graham@mineuk.com>
2015-02-17 18:24:58 +0000

    Tweaked analytics

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -4,6 +4,6 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', '{!! Config::get('analytics.googleid') !!}', '{!! $_SERVER['SERVER_NAME'] !!}');
+  ga('create', '{!! Config::get('analytics.googleid') !!}');
   ga('send', 'pageview');
 </script>
